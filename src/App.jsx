b91cec08d39ab3c5e63c721e22623ac4a0fa7525
TC-Wei <happy533177@gmail.com>
2026-09-11 20:56:25 +0800

    perf: 圖片壓縮至顯示尺寸 2 倍，總量 9.6MB → 844KB，加速手機載入

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import img1 from "./img/img1.jpg";
 import img2 from "./img/img2.jpg";
 import img3 from "./img/img3.jpg";
-import img4 from "./img/img4.png";
+import img4 from "./img/img4.jpg";
 import { Divider, Col, Row, Select, Space, DatePicker, Button } from "antd";
 import { FacebookFilled, InstagramFilled } from "@ant-design/icons";
 import dayjs from "dayjs";
