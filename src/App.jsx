c1a487a077eabc5dc1e58b176b8f24bc9ed010be
TC-Wei <happy533177@gmail.com>
2026-09-12 22:29:32 +0800

    feat: 預約按鈕送出後顯示訂位摘要訊息

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,16 @@ import img1 from "./img/img1.jpg";
 import img2 from "./img/img2.jpg";
 import img3 from "./img/img3.jpg";
 import img4 from "./img/img4.jpg";
-import { Divider, Col, Row, Select, Space, DatePicker, Button } from "antd";
+import {
+  Divider,
+  Col,
+  Row,
+  Select,
+  Space,
+  DatePicker,
+  Button,
+  message,
+} from "antd";
 import { FacebookFilled, InstagramFilled } from "@ant-design/icons";
 import dayjs from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
@@ -297,7 +306,11 @@ function App() {
             </div>
             <div className="reserve">
               <div>{bottomReserve}</div>
-              <button className="reserveBtn" disabled={reservedTime === ""}>
+              <button
+                className="reserveBtn"
+                disabled={reservedTime === ""}
+                onClick={() => message.success(bottomReserve)}
+              >
                 {reservedTime === "" ? "請選擇用餐時段" : "預約"}
               </button>
             </div>
